f04cee249efa76514ceae9c68ac85ec53f727dc4
Sito8943 <sito8943@gmail.com>
2023-05-16 09:54:42 -0700

    parseImageKit

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -48,7 +48,9 @@ export const parseImageKit = (url, quality = "80", width = "", height = "") => {
   const slashSplitter = url.split("/");
   let result = `${slashSplitter[0]}//${slashSplitter[2]}/${
     slashSplitter[3]
-  }/tr:q-${quality}${width.length ? `,w-${width}` : ""}`;
+  }/tr:q-${quality}${width.length ? `,w-${width}` : ""}${
+    height.length ? `,h-${height}` : ""
+  }`;
   for (let i = 4; i < slashSplitter.length; i += 1)
     result += `/${slashSplitter[i]}`;
   return result;
